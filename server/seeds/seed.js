a99b9cf6761f17cfb655ed591d34b4e9aba470d0
Garethus <reyes.grethelmaec@gmail.com>
2023-07-18 20:54:55 +1000

    created sample models

---
+++ server/seeds/seed.js
@@ -0,0 +1,14 @@
+const db = require('../config/connection');
+const { User, Property } = require('../models');
+const userSeeds = require('./userSeeds.json');
+const propertySeeds = require('./propertySeeds.json');
+
+db.once('open', async () => {
+    await Property.deleteMany({});
+    await User.deleteMany({});
+
+    await User.create(userSeeds);
+
+    console.log('all done!');
+    process.exit(0);
+});
