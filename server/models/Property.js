4839afbc5f55afb3227877c97324190a77257c33
Garethus <reyes.grethelmaec@gmail.com>
2023-07-18 20:32:23 +1000

    added propertySchema

---
+++ server/models/Property.js
@@ -0,0 +1,57 @@
+const { Schema, model } = require('mongoose');
+
+const propertySchema = new Schema({
+    streetAddress: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+        trim: true,
+    },
+    suburb: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    state: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    postcode: {
+        type: Number,
+        required: true,
+    },
+    bed: {
+        type: Number,
+        required: true,
+    },
+    bathroom: {
+        type: Number,
+        required: true,
+    },
+    car: {
+        type: Number,
+        required: true,
+    },
+    land: {
+        type: number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    propertyType: {
+        type: String,
+        required: true,
+    },
+    propertyCategory: {
+        type: String,
+        required: true,
+    }
+});
+
+const Property = model('Property', propertySchema);
+
+module.exports = Property;
